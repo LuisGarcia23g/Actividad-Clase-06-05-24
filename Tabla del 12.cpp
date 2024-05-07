91a796de6cee46ea68fe8cce240271ce189393f7
LuisGarcia23g <164560978+LuisGarcia23g@users.noreply.github.com>
2024-05-06 18:46:13 -0600

    Add files via upload
    00114124
    García Ruiz Luis Amílcar
    Actividad de la clase del día 06/05/2024

---
+++ Tabla del 12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int i=1, resultado=0;
+
+while (i<13){
+resultado=12*i;
+ cout << "12 X " << i << " = " << resultado << "\n";
+ i++;
+}
+
+
+    return 0;
+}
